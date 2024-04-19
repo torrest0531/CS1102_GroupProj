856425b215d588900a94004dbe3eb57be1b78600
torres.tsang <torres.tsang@admazes.com>
2024-04-19 13:24:36 +0800

    updated encryption page

--- encryption.js
+++ encryption.js
@@ -1,9 +1,36 @@
+function openSelection(evt, selection) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(selection).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function generateKey() {
+    var key = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    document.getElementById("secretKey").innerHTML = key;
+  }
+
 window.onload = function() {
+
     document.getElementById('dataForm').addEventListener('submit', function(event) {
         event.preventDefault(); // Prevent the form from being submitted
 
         // Get the data from the form
-        var data1 = document.getElementById('secretKey').value;
+        var data1 = document.getElementById('secretKey').innerHTML;
         var data2 = document.getElementById('sendData').value;
 
         document.getElementById('dataForm').reset();
